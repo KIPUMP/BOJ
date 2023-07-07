e98e1bdbb4c94c508b6e7970f907866a4c42ff73
kibeomchoi <kibeom2000@gmail.com>
2023-07-07 18:24:38 +0900

    0607_commit

---
+++ Silver/1260.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=" ")
+
+    for i in sorted(graph[v]):
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited_D = [False] * (N + 1)
+dfs(graph, V, visited_D)
+print()
+
+visited_B = [False] * (N + 1)
+bfs(graph, V, visited_B)
