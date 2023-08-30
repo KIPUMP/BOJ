71b640b5098179ac46e685591f2a60a13bd3b143
kibeomchoi <kibeom2000@gmail.com>
2023-08-30 17:47:35 +0900

    0830_commit

---
+++ Silver/1012.py
@@ -0,0 +1,32 @@
+import sys 
+sys.setrecursionlimit(10000) 
+
+def dfs(x,y) :
+  dx = [0,1,0,-1]
+  dy = [1,0,-1,0]
+
+  for i in range(4) :
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    if (0 <= nx < N) and (0 <= ny < M) :
+      if arr[nx][ny] == 1 :
+        arr[nx][ny] = -1 
+        dfs(nx,ny)
+
+T = int(input())
+for _ in range(T) :
+  M,N,K = map(int,input().split())
+  arr = [[0] * M for _ in range(N)]
+  cnt = 0
+
+  for _ in range(K) :
+    m,n = map(int,input().split())
+    arr[n][m] = 1 
+  
+  for i in range(N) :
+    for j in range(M) :
+      if arr[i][j] > 0 :
+        dfs(i,j)
+        cnt += 1
+  print(cnt)
---
+++ Silver/11724.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+arr = [[] for _ in range(N+1)]
+for _ in range(M) :
+  u,v = map(int,input().split())
+  arr[u].append(v)
+  arr[v].append(u)
+
+def dfs(graph,v,visited) :
+  visited[v] = True
+  for i in graph[v] :
+    if visited[i] == False :
+      dfs(graph,i,visited)
+
+visited = [False] * (N+1)
+cnt = 0
+for i in range(1,N+1) :
+  if visited[i] == False :
+    dfs(arr,i,visited)
+    cnt += 1
+
+print(cnt)
