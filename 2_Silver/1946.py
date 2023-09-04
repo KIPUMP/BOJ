16b620a29cc2f81399f280416bf4644c7fadfebd
kibeomchoi <kibeom2000@gmail.com>
2023-09-04 18:15:14 +0900

    commit_0904

--- 2_Silver/1946.py
+++ 2_Silver/1946.py
@@ -1,21 +1,18 @@
-for _ in range(int(input())) :
-  N = int(input())
-  resumes = []
-  total = N
-  for _ in range(N):
-    first_test,second_test = map(int,input().split())
-    resumes.append((first_test,second_test))
-  resumes.sort()
-  print(resumes)
-  for j in range(N) :
-    t = 0
-    for i in range(len(resumes)-1) :
-      if resumes[t][0] < resumes[(-1)][0] and resumes[t][1] < resumes[(-1)][1] :
-        resumes.remove(resumes[(-1)])
-        total -= 1
-        break
+import sys
+input = sys.stdin.readline
 
-  
-  print(total)
+T = int(input())
 
-  
+for _ in range(T):
+    N = int(input())
+    rank = [list(map(int, input().split())) for _ in range(N)]
+    rank_asc = sorted(rank)
+    top = 0
+    result = 1
+    
+    for i in range(1, len(rank_asc)):
+        if rank_asc[i][1] < rank_asc[top][1]:
+            top = i
+            result += 1
+    
+    print(result)
