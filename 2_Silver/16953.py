bf8f1ab990c78221092a2aacfee5b45ab385757b
kibeomchoi <kibeom2000@gmail.com>
2023-09-10 18:42:33 +0900

    commit_0910

---
+++ 2_Silver/16953.py
@@ -0,0 +1,24 @@
+import sys
+import heapq
+input = sys.stdin.readline
+A, B = map(int, input().split())
+
+def choice_one(cnt,a) :
+  return cnt + 1, a * 2
+
+def choice_two(cnt,a) :
+  return cnt + 1 , a * 10 + 1
+
+
+def solution(a,b) :
+  q = [(0,a)]
+  while q :
+    cnt , a  = heapq.heappop(q)
+    if a == b :
+      return cnt + 1
+    elif  a < b :
+      heapq.heappush(q,choice_one(cnt,a))
+      heapq.heappush(q,choice_two(cnt,a))
+  return - 1
+
+print(solution(A,B))
