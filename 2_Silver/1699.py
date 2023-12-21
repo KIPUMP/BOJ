c6bc2d5c658d90977e7b7d74d44755329f187c0e
kibeom <kibeom2000@naver.com>
2023-12-22 07:15:33 +0900

    commit_1221

--- 2_Silver/1699.py
+++ 2_Silver/1699.py
@@ -1,6 +1,12 @@
 n = int(input())
-d = [0] * (n+1)
-d[1] = 1
-d[2] = 2
+d = [i for i in range(n+1)]
 
-for i in range(3,n+1) : 
+for i in range(1,n+1) :
+    for j in range(1,i) :
+        if j**2 > i :
+            break
+        else :
+            if d[i] > d[i - (j**2)] + 1:
+                d[i] = d[i - (j**2)] + 1
+                
+print(d[n])
