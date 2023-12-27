cb9fd1322a4e2cb29384b0707c5355393c2efca8
kibeomchoi <kibeom2000@gmail.com>
2023-12-27 17:01:44 +0900

    commit_1227

---
+++ 2_Silver/1475.py
@@ -0,0 +1,13 @@
+n = list(input().rstrip())
+result = [0] * 10
+for i in n :
+  if int(i) == 6 or int(i) == 9 :
+    if result[6] <= result[9] :
+      result[6] += 1
+    else :
+      result[9] += 1
+
+  else :
+    result[int(i)] += 1
+
+print(max(result))
