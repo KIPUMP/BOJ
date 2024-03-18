3e716fb3b1def13452b1d0fe4822d94541b6001e
kibeomchoi <kibeom2000@gmail.com>
2024-03-18 21:36:26 +0900

    commit_0318

---
+++ 2_Silver/1182.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+n,s = map(int,input().split())
+arr = list(map(int,input().split()))
+
+result = 0
+for i in range(1,n+1) :
+  for j in combinations(arr,i) :
+    if sum(j) == s :
+      result += 1 
+
+
+print(result)
