eb383b066a6d37327025ad62ffad7819007b0102
kibeomchoi <kibeom2000@gmail.com>
2024-05-11 14:09:06 +0900

    commit_0511

--- 2_Silver/7785.py
+++ 2_Silver/7785.py
@@ -1,16 +1,17 @@
 n = int(input())
-log = set()
+log = dict()
+
 
 for i in range(n) :
   name, work = input().split()
 
   if work == 'enter' :
-    log.add(name)
+    log[name] = True
 
   elif work == 'leave' :
-    log.discard(name)
+    del log[name]
 
+log = sorted(log, reverse=True)
 
-log = sorted(log,reverse=True)
 for i in log :
   print(i)
