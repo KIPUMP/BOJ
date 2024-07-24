6b78a8d96d8644d06ebf2992b506a5041f83c683
KIPUMP <kibeom2000@naver.com>
2024-07-25 07:43:44 +0900

    commit_filerefactoring

--- 2_Silver/1158.py
+++ 2_Silver/1158.py
@@ -1,17 +1,15 @@
 from collections import deque
+
 n,k = map(int,input().split())
-queue = deque()
+students = [i for i in range(1,n+1)]
+queue = deque(students)
+
 result = []
-
-for i in range(1,n+1) :
-  queue.append(i)
-
-while queue :
-  for _ in range(k-1) :
-    queue.append(queue.popleft())
-  
-  x = queue.popleft()
-  result.append(x)
-
+while queue :                   
+    for _ in range(k-1) :                   # 가장 첫 값을 k번째로 바꿈
+        queue.append(queue.popleft())
+    
+    result.append(queue.popleft())          # k 번째 결과 값 넣기 
+    
 print(str(result).replace('[','<').replace(']','>'))
-  
+    
