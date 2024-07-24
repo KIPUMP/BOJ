8c88a1f9b005f1ff764d037261a975bd9f1b774e
kibeomchoi <kibeom2000@gmail.com>
2024-07-24 15:42:59 +0900

    commit_0724(sparta07)

--- 0_Wrong answer notes/1406.py
+++ 0_Wrong answer notes/1406.py
@@ -1,36 +1,32 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-n = list(input().strip())                       # 문자열
-m = int(input())
-idx = len(n)                                    # 커서 인덱스
+n = list(input().strip())  
 
-for i in range(int(m)) :
-    command = list(input().split())             # 명령 
-    
-    left = n[:idx]
-    right = n[idx:]
-    
-    if command[0] == 'L' :                      # 커서 왼쪽 이동
-        if idx == 0 :
-            continue
-        idx -= 1
+left = deque(n)
+right = deque()
 
-    elif command[0] == 'D' :                    # 커서 오른쪽 이동
-        if idx == len(n):
-            continue
-        idx += 1
+for _ in range(int(input())):
+    command = list(input().split())                 # 명령
+
+    if command[0] == 'L' :
+        if left :
+            right.appendleft(left.pop())           #왼쪽 리스트 끝의 원소 오른쪽에 삽입   
+
+    elif command[0] == 'D' :
+        if right :
+            left.append(right.popleft())           #오른쪽 리스트 완쪽 끝의 원소를 왼쪽리스트에 삽입 
         
-    elif command[0] == 'B' :                    # 커서 왼쪽의 문자 제거
-        if idx == 0 :
-            continue
-        left.pop()
-        idx -= 1
-            
-    elif command[0] == 'P' :                     # 커서 왼쪽에 문자 추가
+    elif command[0] == 'B' :
+        if left :
+            left.pop()                            # left에 원소 있다면 제거
+
+    elif command[0] == "P" :
         left.append(command[1])
-        idx += 1
-        
-    n = left+right
 
-print(*n,sep="")
+result = left + right
+
+print(*result,sep="")
+
+
