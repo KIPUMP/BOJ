947291fb0ed414bef4f449c0c55a74370c8a8871
kibeomchoi <kibeom2000@gmail.com>
2024-07-25 12:03:42 +0900

    commit_test file

--- 0_Wrong answer notes/test.py
+++ 0_Wrong answer notes/test.py
@@ -1,4 +1,5 @@
 import heapq
+from collections import Counter
 
 min_heap = []                   # 최소 힙
 heapq.heappush(min_heap,5)
@@ -11,3 +12,9 @@ heapq.heappush(max_heap,-5)
 heapq.heappush(max_heap,-1)
 heapq.heappush(max_heap,-3)
 print(-heapq.heappop(max_heap))
+
+
+arr = [1,2,2,3,3,3]
+
+arr_cnt = Counter(arr)
+print(arr_cnt)
