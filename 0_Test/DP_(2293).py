3d64dc9a49031de8f4918298bf0c50d9cea87522
KIPUMP <kibeom2000@naver.com>
2024-09-11 19:18:44 +0900

    commit_0911

--- 0_Test/DP_(2293).py
+++ 0_Test/DP_(2293).py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/2293
+# https://velog.io/@jxlhe46/%EB%B0%B1%EC%A4%80-2293%EB%B2%88.-%EB%8F%99%EC%A0%84-1-bfi120m5
 
 n,k = map(int,input().split())
 coins = [int(input()) for _ in range(n)]
@@ -6,11 +7,15 @@ dp = [0] * (k+1)
 
 coins.sort()
 
-for i in coins :
-    dp[i] += 1
-    
-for i in range(k+1) :
-    dp[i] +=
+coins.reverse()
+
+dp[0] = 1
+
+for i in range(n) :
+    for j in range(coins[i], k+1) :
+        dp[j] += dp[j - coins[i]]
+
+print(dp[k])
 
 
-
+# 맞았습니다!!	31120	164
